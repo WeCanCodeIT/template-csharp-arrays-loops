5d31487f127ba3eb93f67b5ecb9b999e3f09f28d
Jen E <jenevans3ten@gmail.com>
2020-05-08 11:58:49 -0400

    WIP switch statement for all exercises and first 2 methods added

--- ArraysAndLoopsExercises/Program.cs
+++ ArraysAndLoopsExercises/Program.cs
@@ -10,27 +10,16 @@ namespace ArraysAndLoopsExercises
             while (keepThinking)
             {
                 Console.Clear();
-                Console.WriteLine("COMPUTATIONAL THINKING EXERCISES\n");
+                Console.WriteLine("ARRAYS AND LOOPS EXERCISES\n");
                 Console.WriteLine("Which exercise would you like to run?");
-                Console.WriteLine("1. Are These Integers Equal?");
-                Console.WriteLine("2. Even or Odd?");
-                Console.WriteLine("3. Vowel or Consonant?");
-                Console.WriteLine("4. Find the Largest Number");
-                Console.WriteLine("5. Divisible by 3");
-                Console.WriteLine("6. Reverse Counting");
-                Console.WriteLine("7. Restaurant Bill");
-                Console.WriteLine("8. Age Category");
-                Console.WriteLine("9. Words to Digits");
-                Console.WriteLine("10. Which Name is Longer?");
-                Console.WriteLine("11. Are these Numbers the Same?");
-                Console.WriteLine("12. Name and Place of Birth");
-                Console.WriteLine("13. Sort the Numbers");
-                Console.WriteLine("14. Shopping List");
-                Console.WriteLine("15. What's the Longest Word?");
-                Console.WriteLine("16. What's the Second to Last Word?");
-                Console.WriteLine("17. Which Day of the Week?");
-                Console.WriteLine("18. Print a Triangle");
-                Console.WriteLine("19. Guess the Magic Number");
+                Console.WriteLine("1. Sort the Numbers");
+                Console.WriteLine("2. Which Day of the Week?");
+                Console.WriteLine("3. Print a Triangle");
+                Console.WriteLine("4. Guess the Magic Number");
+                Console.WriteLine("5. Print Numbers in Reverse Order");
+                Console.WriteLine("6. Print the Square of Even Numbers");
+                Console.WriteLine("7. Find the Smallest Number in an Array");
+                Console.WriteLine("8. Common Values");
                 Console.WriteLine("Press Q to quit");
 
                 string userChoice = Console.ReadLine().ToLower();
@@ -38,61 +27,28 @@ namespace ArraysAndLoopsExercises
                 switch (userChoice)
                 {
                     case "1":
-                        AreTheseIntegersEqual();
+                        SortTheNumbers();
                         break;
                     case "2":
-                        EvenOrOdd();
-                        break;
-                    case "3":
-                        VowelOrConsonant();
-                        break;
-                    case "4":
-                        FindLargestNumber();
-                        break;
-                    case "5":
-                        DivisibleBy3();
-                        break;
-                    case "6":
-                        ReverseCounting();
-                        break;
-                    case "7":
-                        RestaurantBill();
-                        break;
-                    case "8":
-                        AgeCategory();
-                        break;
-                    case "9":
-                        WordsToDigits();
-                        break;
-                    case "10":
-                        WhichNameIsLonger();
-                        break;
-                    case "11":
-                        AreNamesSame();
-                        break;
-                    case "12":
-                        NameAndBirthplace();
-                        break;
-                    case "13":
-                        SortNumbers();
-                        break;
-                    case "14":
-                        ShoppingList();
-                        break;
-                    case "15":
-                        LongestWord();
-                        break;
-                    case "16":
-                        SecondToLastWord();
-                        break;
-                    case "17":
                         WhichDayOfWeek();
                         break;
-                    case "18":
+                    case "3":
                         PrintTriangle();
                         break;
-                    case "19":
-                        GuessMagicNumber();
+                    case "4":
+                        GuessTheMagicNumber();
+                        break;
+                    case "5":
+                        NumbersInReverse();
+                        break;
+                    case "6":
+                        SquareOfEvenNumbers();
+                        break;
+                    case "7":
+                        SmallestNumberInArray();
+                        break;
+                    case "8":
+                        CommonValues();
                         break;
                     case "q":
                         keepThinking = false;
@@ -104,21 +60,37 @@ namespace ArraysAndLoopsExercises
             }
         }
 
-        static void AreTheseIntegersEqual()
+        static void SortTheNumbers()
         {
-            // Write a console application to ask the user for two integers. 
-            // Check and see if the two integers are equal to each other.
-            // If they are, inform the the user that the numbers are equal,
-            // else inform the user that the numbers are not equal
+            // Ask 10 people for their favorite numbers and create an array of those numbers. 
+            // Order the numbers using the sort method. Print the first and last numbers.
 
             Console.Clear();
-            Console.WriteLine("ARE THESE INTEGERS EQUAL?\n");
+            Console.WriteLine("SORT THE NUMBERS\n");
 
             // Enter your solution here
 
             Console.Write("Press enter to return to the Main Menu");
             Console.ReadLine();
         }
+
+        static void WhichDayOfWeek()
+        {
+            // Declare and initialize a string array with the 7 days of the week. 
+            // Print the number corresponding to each day and the day of the week. (1 = Sunday, 2 = Monday, etc.)
+
+            // Ask the user for a number between 1 - 7.
+            // Print the day of the week that corresponds to the number
+            
+            Console.Clear();
+            Console.WriteLine("WHICH DAY OF THE WEEK?\n");
+
+            // Enter your solution here
+
+            Console.Write("Press enter to return to the Main Menu");
+            Console.ReadLine();
+        }
+
 
     }
 }
