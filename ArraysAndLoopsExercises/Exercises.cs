615b4f13b5dec31f3c82df4051b824dcd5dc8bae
daviswcci <79462946+daviswcci@users.noreply.github.com>
2022-05-24 15:29:20 -0400

    updated to .net 6

---
+++ ArraysAndLoopsExercises/Exercises.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraysAndLoopsExercises
+{
+    internal class Exercises
+    {
+        public static void SortTheNumbers()
+        {
+            // Ask 10 people for their favorite numbers and create an array of those numbers. 
+            // Order the numbers using the sort method. Print the first and last numbers.
+
+            Console.Clear();
+            Console.WriteLine("SORT THE NUMBERS\n");
+
+            // Enter your solution here
+
+            Console.Write("Press enter to return to the Main Menu");
+            Console.ReadLine();
+        }
+
+        public static void WhichDayOfWeek()
+        {
+            // Declare and initialize a string array with the 7 days of the week. 
+            // Print the number corresponding to each day and the day of the week. (1 = Sunday, 2 = Monday, etc.)
+
+            // Ask the user for a number between 1 - 7.
+            // Print the day of the week that corresponds to the number
+
+            Console.Clear();
+            Console.WriteLine("WHICH DAY OF THE WEEK?\n");
+
+            // Enter your solution here
+
+            Console.Write("Press enter to return to the Main Menu");
+            Console.ReadLine();
+        }
+
+        public static void SmallestNumberInArray()
+        {
+            // Ask the user for 5 integers and save them in an array. 
+            // Find the smallest number in the array and write it to the screen.
+
+            Console.Clear();
+            Console.WriteLine("FIND THE SMALLEST NUMBER IN AN ARRAY\n");
+
+            // Enter your solution here
+
+            Console.Write("Press enter to return to the Main Menu");
+            Console.ReadLine();
+        }
+
+
+        public static void ShoppingList()
+        {
+            // Initialize an array with 4 food items. Print this shopping list to the console.
+
+            Console.Clear();
+            Console.WriteLine("SHOPPING LIST\n");
+
+            // Enter your solution here
+
+            Console.Write("Press enter to return to the Main Menu");
+            Console.ReadLine();
+        }
+
+        public static void LongestWord()
+        {
+            // Ask the user for a phrase consisting of 1-4 words (no punctuation).
+            // Print the longest word in the phrase. If 2 words have the same length, print both.
+
+            Console.Clear();
+            Console.WriteLine("WHAT'S THE LONGEST WORD?\n");
+
+            // Enter your solution here
+
+            Console.Write("Press enter to return to the Main Menu");
+            Console.ReadLine();
+        }
+
+        public static void SecondToLastWord()
+        {
+            // Ask the user for a sentence or phrase consisting of at least 4 words.
+            // Find the second to last word and print it.
+
+            Console.Clear();
+            Console.WriteLine("WHAT'S THE SECOND TO LAST WORD?\n");
+
+            // Enter your solution here
+
+            Console.Write("Press enter to return to the Main Menu");
+            Console.ReadLine();
+        }
+
+        public static void PrintTriangle()
+        {
+            // Ask the user for a number between 3 and 6. 
+            // Print one asterisk to the screen. 
+            // On the next line print 2 asterisks, and continue until the users number is reached.
+
+            Console.Clear();
+            Console.WriteLine("PRINT A TRIANGLE\n");
+
+            // Enter your solution here
+
+            Console.Write("Press enter to return to the Main Menu");
+            Console.ReadLine();
+        }
+
+        public static void CommonValues()
+        {
+            // You are given 3 arrays of the same size, where 1 is empty and 2 have values. 
+            // Use nested loops to find the values that are common between the 2 arrays.
+
+            Console.Clear();
+            Console.WriteLine("COMMON VALUES\n");
+
+            // Enter your solution here
+
+            Console.Write("Press enter to return to the Main Menu");
+            Console.ReadLine();
+        }
+
+    }
+}
